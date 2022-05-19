ab9699219a6a9d2ec0693e366a3fed9942cadccb
Jihyeon Lee <101785677+wlgus5704@users.noreply.github.com>
2022-05-20 00:32:15 +0900

    Update Payment.java

--- Payment.java
+++ Payment.java
@@ -1,11 +1,14 @@
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
 import javax.swing.*;
 
 public class Payment {
 
-	ImageIcon Background= new ImageIcon("C:\\Users\\wlgus\\OneDrive\\바탕 화면\\KIOSK\\004.jpg");
+	ImageIcon Background= new ImageIcon("images/004.jpg");
 	JFrame f=new JFrame("융다방 키오스크"); 
-	String random_f;
+	String total_Label;
 	
 	Payment(){
 		
@@ -21,36 +24,56 @@ public class Payment {
         f.setVisible(true);//창을 화면에 나타낼 것인지설정
         f.setResizable(false); //창고정
         f.setLayout(null); //레이아웃 null(절대위치 설정할거라서)
-	
         
-        JLabel op = new JLabel("주문금액 : "); //라벨링
+        
+        
+        //테이크 아웃 할인
+       if(Main.Takeout == true) {
+    	  Menu.totalPrice -=1000;
+    	  total_Label = "주문금액 :   "+ Menu.totalPrice+"원 (Take out - 1000)";}
+       else total_Label = "주문금액 :   "+ Menu.totalPrice;
+        
+       //주문금액 
+        JLabel op = new JLabel(total_Label); //라벨링
 		f.add(op);//프레임에 라벨링 추가
-		op.setBounds(280, 150, 100,30);//절대위치지정
+		op.setBounds(280, 150, 350,30);//절대위치지정
 		op.setFont(op.getFont().deriveFont(18.0f)); //글씨 크기지정
 		
-		JButton button0 = new JButton("CARD");//버튼추가
-		button0.setBounds(600,480,130,40);//버튼 위치 및 크기설정
-		f.add(button0);
+		// 랜덤이벤트 문자열에 추가
+		if(Random.Random.equals("꽝")|Random.Random=="증정품 없음") { 
+			//꽝 당첨시 내역에 미기재
+		}
+		else Menu.order_list += Random.Random+" 증정";
 		
-		if(Random.Random.equals("꽝")) { //꽝 당첨시 내역에 미기재
-				random_f = "";}
-		else random_f = Random.Random;
+		//문자열 JLabel에 맞게 변환
+        String st = "<html>"+ Menu.order_list+"</html>";
+        st = st.replaceAll("\n", "<br>");
 		
-	JLabel ol = new JLabel("주문내역 : "+"내용 "+random_f); //라벨링 (**임시**)
+		JLabel o = new JLabel("주문내역 : "); //라벨링
+		f.add(o);//프레임에 라벨링 추가
+		o.setBounds(280, 180, 150,30);//절대위치지정
+		o.setFont(o.getFont().deriveFont(18.0f)); //글씨 크기지정
+		
+		JLabel ol = new JLabel(st); //라벨링
 		f.add(ol);//프레임에 라벨링 추가
-		ol.setBounds(280, 180, 200,30);//절대위치지정
+		ol.setBounds(380, 185, 400,100);//절대위치지정
 		ol.setFont(ol.getFont().deriveFont(18.0f)); //글씨 크기지정
 		
 		JButton button0 = new JButton("CARD");//버튼추가
 		f.add(button0);
 		button0.setBounds(600,480,130,40);//버튼 위치 및 크기설정
+		button0.addActionListener(new MyActionListener0());
 		
-		button0.addActionListener(new MyActionListener_c());
+		JButton button1 = new JButton("BACK");//버튼추가
+		f.add(button1);
+		button1.setBounds(50,480,130,40);//버튼 위치 및 크기설정
+		button1.addActionListener(new MyActionListener1());
+		
 		
 		
 	}	
 	
-	class MyActionListener_c implements ActionListener{
+	class MyActionListener0 implements ActionListener{
 		public void actionPerformed(ActionEvent e) {
 			JButton b = (JButton)e.getSource();
 			if (b.getText().equals("CARD")) {
@@ -61,14 +84,24 @@ public class Payment {
 				new_f.setResizable(false); //창고정
 				new_f.setLayout(null); //레이아웃 null(절대위치 설정할거라서)
 				
-				JLabel card_m = new JLabel("<html>결제가 완료되었습니다.<br/> 카드를 제거해주세요<html>"); //라벨링
+				JLabel card_m = new JLabel("<html>결제가 완료되었습니다.<br> 카드를 제거해주세요<html>"); //라벨링
 				new_f.add(card_m);//프레임에 라벨링 추가
 				card_m.setBounds(110, 40, 200,80);//절대위치지정
 				card_m.setFont(card_m.getFont().deriveFont(15.0f)); //글씨 크기지정
 				}
 	}}
 	
+	class MyActionListener1 implements ActionListener{
+		public void actionPerformed(ActionEvent e) {
+			JButton b = (JButton)e.getSource();
+			if (b.getText().equals("BACK")) {
+				Menu.totalPrice = 0;
+				Menu.order_list = "";
+				
+			new Menu();
+			f.setVisible(false);}}}
+	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		Payment pay = new Payment();
+		Payment p = new Payment();
 }}
