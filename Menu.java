4d0c18a5e72c27c5dda05c947b4480029312d8ae
Jihyeon Lee <101785677+wlgus5704@users.noreply.github.com>
2022-05-23 10:34:05 +0900

    sold out 기능추가

--- Menu.java
+++ Menu.java
@@ -22,19 +22,20 @@ public class Menu extends JFrame {
 		JPanel Back = new JPanel() { // 프레임 이미지 background만들기 위해 판넬생성
 			public void paintComponent(Graphics g) {
 				g.drawImage(image.getImage(), 0, 0, null);
-				setOpaque(false); // 그림을 표시하게 설정
+				setOpaque(false);    // 그림을 표시하게 설정
 				super.paintComponent(g);
 			}
 		};
-
-		f.setContentPane(Back);// 프레임을 background로 바꿈
+		
+		// 프레임을 background로 바꿈
+		f.setContentPane(Back);
 
 		// 소제목
 		JLabel A = new JLabel("COFFEE"); // 라벨링
 		JLabel B = new JLabel("NON-COFFEE");
 		JLabel C = new JLabel("DESERT");
 		f.add(A);
-		A.setBounds(35, 35, 100, 30);// 절대위치지정
+		A.setBounds(35, 35, 100, 30);   // 절대위치지정
 		f.add(B);
 		B.setBounds(35, 200, 100, 30);
 		f.add(C);
@@ -52,14 +53,85 @@ public class Menu extends JFrame {
 		int xx = 35; //이미지
 		int yy = 65;
 		
+		//품절메뉴
+		String [] soldout = {"","","","","","","","",""};
+		//아메리카노, 스위트라떼, 카페모카, 밀크티, 초코라떼, 녹차, 마카롱, 치즈케이크, 크로플
+		
+		for(int j =0; j<9; j++) {
+			if(soldout[j] == Menu_name[j]) {
+			
+				switch(j) {
+				case 0:
+					Menu_name[0] = Menu_name[1];
+					Menu_name[1] = Menu_name[2];
+					Menu_name[2] = "";
+					price[0] = price[1];
+					price[1] = price[2];
+					price[2] = 0;
+					break;
+				case 1:
+					Menu_name[1] = Menu_name[2];
+					Menu_name[2] = "";
+					price[1] = price[2];
+					price[2] = 0;
+					break;
+				case 2: 	
+					Menu_name[2] = "";
+					price[2] = 0;
+					break;
+				case 3:
+					Menu_name[3] = Menu_name[4];
+					Menu_name[4] = Menu_name[5];
+					Menu_name[5] = "";
+					price[3] = price[4];
+					price[4] = price[5];
+					price[5] = 0;
+					break;
+				case 4:
+					Menu_name[4] = Menu_name[5];
+					Menu_name[5] = "";
+					price[4] = price[5];
+					price[5] =0;
+					break;
+				case 5:
+					Menu_name[5] = "";
+					price[5] = 0;
+					break;
+				case 6:
+					Menu_name[6] = Menu_name[7];
+					Menu_name[7] = Menu_name[8];
+					Menu_name[8] = "";
+					price[6] = price[7];
+					price[7] = price[8];
+					price[8] = 0;
+					break;
+				case 7:
+					Menu_name[7] = Menu_name[7];
+					Menu_name[8] = "";
+					price[7] = price[8];
+					price[8] =0;
+					break;
+				case 8:
+					Menu_name[8] = "";
+					price[8] = 0;
+					break;
+					
+				}
+				
+				}}
+			
+
 		for (int i = 0 ; i < Menu_name.length; i++) {
+			
+			
 			menuLabel[i] = new JLabel(Menu_name[i]);
 			f.add(menuLabel[i]);
-			menuLabel[i].setBounds(x, y, 60, 26);
+			menuLabel[i].setBounds(x, y, 65, 26);
 			
+			if(price[i] != 0) {
 			priceLabel[i] = new JLabel(Integer.toString(price[i]));
 			f.add(priceLabel[i]);
-			priceLabel[i].setBounds(x, y+20, 60, 25);
+			priceLabel[i].setBounds(x, y+20, 60, 25);}
 			
 			
 			menuImages[i] = new JLabel(new ImageIcon("images/"+ Menu_name[i] +"(정사각형).jpg"));
@@ -67,19 +139,15 @@ public class Menu extends JFrame {
 			menuImages[i].setBounds(xx, yy, 80, 80);
 			
 			if (i%3 ==2 ) {  // 인덱스 2, 5, 8
-				x = 125; //JLabel
-				y += 165;
+				x = 125;     //JLabel
+				y += 165; 
 				
-				xx = 35; //이미지
-				yy += 165;
-				
-			}
+				xx = 35;     //이미지
+				yy += 165;   }
 			
 			else if (i%3 == 0 || i%3 == 1) {  // 인덱스 1, 2, 4, 5, 7, 8
 				x += 190;
-				xx += 185;
-		
-			}
+				xx += 185; }
 		}
 		
 
@@ -89,8 +157,8 @@ public class Menu extends JFrame {
 		f.add(button2);
 		button2.addActionListener(new MyActionListener2());// 액션리스너 추가
 
-		JButton button = new JButton("ORDER");// 버튼추가
-		button.setBounds(625, 480, 100, 40);// 버튼 위치 및 크기설정
+		JButton button = new JButton("ORDER");            // 버튼추가
+		button.setBounds(625, 480, 100, 40);              // 버튼 위치 및 크기설정
 		f.add(button);
 		button.addActionListener(new MyActionListener3());// 액션리스너 추가
 
@@ -137,30 +205,59 @@ public class Menu extends JFrame {
 
 		croffle_s.addChangeListener(new SpinnerListener8());
 
-		f.add(americano_s);
+		
 		americano_s.setBounds(125, 110, 50, 30);
-		f.add(sweetlatte_s);
 		sweetlatte_s.setBounds(320, 110, 50, 30);
-		f.add(cafemocha_s);
 		cafemocha_s.setBounds(500, 110, 50, 30);
-		f.add(milktea_s);
 		milktea_s.setBounds(125, 275, 50, 30);
-		f.add(chocolatte_s);
 		chocolatte_s.setBounds(315, 275, 50, 30);
-		f.add(greentea_s);
 		greentea_s.setBounds(500, 275, 50, 30);
-		f.add(macaron_s);
 		macaron_s.setBounds(125, 445, 50, 30);
-		f.add(cheesecake_s);
 		cheesecake_s.setBounds(315, 445, 50, 30);
-		f.add(croffle_s);
 		croffle_s.setBounds(500, 445, 50, 30);
 
 		f.setSize(800, 600);
 		f.setLayout(null);
 		f.setVisible(true);
 
-
+		for(int i =0; i<9; i++) {
+			if(price[i] == 0) {
+				continue;}
+			else {
+				switch(i) {
+				case 0:
+					f.add(americano_s);
+					break;
+				case 1:
+					f.add(sweetlatte_s);
+					break;
+				case 2: 
+					f.add(cafemocha_s);
+					break;
+				case 3:
+					f.add(milktea_s);
+					break;
+				case 4:
+					f.add(chocolatte_s);
+					break;
+				case 5:
+					f.add(greentea_s);
+					break;
+				case 6:
+					f.add(macaron_s);
+					break;
+				case 7:
+					f.add(cheesecake_s);
+					break;
+				case 8:
+					f.add(croffle_s);
+					break;
+				}
+			}}
+		
+		
+		
+		
 	}
 
 	void total() {
@@ -177,7 +274,12 @@ public class Menu extends JFrame {
 				;
 			}
 		}
+		
+		
+		
 	}
+	
+	
 
 	class SpinnerListener0 implements ChangeListener {
 		public void stateChanged(ChangeEvent evt) {
